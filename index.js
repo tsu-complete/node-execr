bfcf74bd7c6880a390d9e990ec7eb11cb84c0f16
tsukumo <tsu.complete@gmail.com>
2015-07-07 02:51:45 -0400

    Add project files and package information
    Project specified runner to be aliased as binary execr

---
+++ index.js
@@ -0,0 +1,128 @@
+
+(function ( cp ) {
+    "use strict";
+
+    var execr, extend;
+
+    /**
+     * extends a source onto a target
+     * @private
+     * @function extend
+     * @param {Object} target onto what to extend
+     * @param {Object} source what to pull from
+     * @return {Object} target object
+     */
+    extend = function ( target, source ) {
+        var key;
+        for (key in source) {
+            if (source.hasOwnProperty(key)) {
+                target[key] = source[key];
+            }
+        }
+        return target;
+    };
+
+    /**
+     * execute a command with the passed parameters
+     * @function execr
+     * @param {String|Array<String>} command what to execute
+     * @param {Object} [options={}] fine tune controls
+     * @param {String}   [options.prefix=LINUX_PREFIX] env syntax prefix
+     * @param {String}   [options.suffix=LINUX_SUFFIX] env syntax suffix
+     * @param {Function} [options.stdout=null] called with 1> output
+     * @param {Function} [options.stderr=null] called with 2> output
+     * @param {Function} [options.failed=null] called with caught errors
+     * @param {Boolean}  [options.isfile=false] true if command is a file
+     */
+    module.exports = execr = function ( command, options ) {
+        var key, callback, pregex, sufgex;
+
+        options = extend({
+            prefix: execr.LINUX_PREFIX
+        ,   suffix: execr.LINUX_SUFFIX
+        ,   stdout: null
+        ,   stderr: null
+        ,   failed: null
+        ,   isfile: false
+        }, options || { });
+
+        pregex = new RegExp(options.prefix);
+        sufgex = new RegExp(options.suffix);
+
+        if (command instanceof Array) {
+            command = command.join(" ");
+        }
+
+        for (key in process.env) {
+            if (!(pregex.test(command) && sufgex.test(command))) {
+                break; // exit early for efficiency
+            }
+            command = command.replace(
+                new RegExp(options.prefix + key + options.suffix, "gi")
+            ,   process.env[key]
+            );
+        }
+
+        callback = function ( error, stdout, stderr ) {
+            if (error && "function" === typeof options.failed) {
+                options.failed(error);
+            }
+            if (stdout && "function" === typeof options.stdout) {
+                options.stdout(stdout);
+            }
+            if (stderr && "function" === typeof options.stderr) {
+                options.stderr(stderr);
+            }
+        };
+
+        if (options.isfile) {
+            options.isfile = command.match(/\S+|\S+".*"/g)
+            .slice(2).map(function ( item ) {
+                return item
+                    .replace(/^(.*)"/g, "$1")
+                    .replace(/"(.*)$/g, "$1");
+            });
+            command = command.substr(0, command.indexOf(" "));
+            cp.execFile(command, options.isfile, callback);
+        } else {
+            cp.exec(command, callback);
+        }
+
+    };
+
+    /**
+     * linux style pattern prefix
+     * @memberof execr
+     * @member LINUX_PREFIX
+     * @static
+     */
+    execr.LINUX_PREFIX = "\\$";
+
+    /**
+     * linux style pattern suffix
+     * @memberof execr
+     * @member LINUX_SUFFIX
+     * @static
+     */
+    execr.LINUX_SUFFIX = "";
+
+    /**
+     * windows style pattern prefix
+     * @memberof execr
+     * @member WINDOWS_PREFIX
+     * @static
+     */
+    execr.WINDOWS_PREFIX = "%";
+
+    /**
+     * windows style pattern suffix
+     * @memberof execr
+     * @member WINDOWS_SUFFIX
+     * @static
+     */
+    execr.WINDOWS_SUFFIX = "%";
+
+})(
+    require("child_process")
+);
+
---
+++ runner.js
@@ -0,0 +1,63 @@
+
+(function ( getopt, execr ) {
+    "use strict";
+
+    var opt, options;
+
+    opt = getopt.create([
+        ["" , "prefix=ARG", "env syntax prefix"]
+    ,   ["" , "suffix=ARG", "env syntax suffix"]
+    ,   ["v", "verbose"   , "print exec-ed stdout/stderr"]
+    ,   ["s", "silent"    , "exit gracefully on error"]
+    ,   ["f", "file"      , "command is a script to be run"]
+    ,   ["h", "help"      , "display this screen"]
+    ,   ["" , "version"   , "display version"]
+    ]).bindHelp().parseSystem();
+
+    if (opt.options.version) {
+        console.log(require("./package.json").version);
+        process.exit(0);
+    }
+
+    options = {  };
+
+    if (opt.options.file) {
+        options.isfile = true;
+    }
+    if (opt.options.prefix != null) {
+        options.prefix = opt.options.prefix;
+    }
+    if (opt.options.suffix != null) {
+        options.suffix = opt.options.suffix;
+    }
+
+    options.failed = function ( error ) {
+        if (opt.options.verbose) {
+            console.error(error.message);
+            if (error.killed) {
+                console.error("[killed: " + error.signal + "]");
+            }
+        }
+        if (!opt.options.silent) {
+            process.exit(error.code);
+        }
+    };
+
+    if (opt.options.verbose) {
+        options.stdout = function ( data ) {
+            console.log("stdout:\n" + data);
+        };
+        options.stderr = function ( data ) {
+            console.error("stderr:\n" + data);
+        };
+
+        console.log("running: ", opt.argv);
+    }
+
+    execr(opt.argv, options);
+
+})(
+    require("node-getopt")
+,   require("./index")
+);
+
